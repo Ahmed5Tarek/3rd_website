f858b09d4f75c90af0cd7ce21ec874176a17e0fe
Ahmed Tarek Mohamed <111061860+Ahmed5Tarek@users.noreply.github.com>
2022-10-07 15:18:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// var Num=prompt("Enter The Number of Students")
+// var Arr=[]
+// for(var x=0; x<Num; x++){
+// var Name=prompt("Enter The Student Name")
+// Arr[x]=Name;
+// }
+// console.log(Arr)
